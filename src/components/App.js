f40b7785dfadd38900ad04fe76a6e8483c0930a8
Sungmin Yi <ssuyi@ucdavis.edu>
2017-10-16 15:55:35 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class APP extends Component {
+    render() {
+        return (
+            <div className="APP">
+
+            </div>
+        );
+    }
+}
+
+export default APP
